fd1b0c519373faa6dc834a19fd88f10c61478c2e
Franciszek <franekkociszewski33@gmail.com>
2020-05-29 20:22:15 +0200

    removind date label and date entry element

--- main.py
+++ main.py
@@ -12,17 +12,12 @@ class Todolist:
         self.label_task_name = Label(self.task_adding_frame, text="task name:")
         self.input_task_name_entry = Entry(self.task_adding_frame)
 
-        self.deadline_date_label = Label(self.task_adding_frame, text="deadline date:")
-        self.deadline_date_input = Entry(self.task_adding_frame)
     
         self.add_task_button = Button(self.task_adding_frame, text="add task", command=self.put_task_to_box)
         
 
         self.label_task_name.grid(row=0, column=0)
         self.input_task_name_entry.grid(row=1, column=0)
-
-        self.deadline_date_label.grid(row=2, column=0)
-        self.deadline_date_input.grid(row=3, column=0)
 
         self.add_task_button.grid(row=4, column=0)
 
