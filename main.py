491d0f2d8ef0ed35919b28f71bdde5a900d6c2ff
Franciszek <franekkociszewski33@gmail.com>
2020-05-29 16:21:22 +0200

    building task adding frame

--- main.py
+++ main.py
@@ -5,10 +5,30 @@ class Todolist:
         self.master = master
         self.master.title(str(window_title))
         
-        
+        self.task_adding_frame = LabelFrame(self.master, text="add task", padx=30, pady=20)
+        self.task_adding_frame.pack(padx=30, pady=20)
+
+       
+        self.help_label_task_name = Label(self.task_adding_frame, text="task name")
+        self.input_task_name_entry = Entry(self.task_adding_frame)
+
+        self.deadline_date_label = Label(self.task_adding_frame, text="deadline date")
+        self.deadline_date_input = Entry(self.task_adding_frame)
+
+        self.add_task_button = Button(self.task_adding_frame, text="add task")
+
+
+        self.help_label_task_name.grid(row=0, column=0)
+        self.input_task_name_entry.grid(row=0, column=1)
+
+        self.deadline_date_label.grid(row=1, column=0)
+        self.deadline_date_input.grid(row=1, column=1)
+
+        self.add_task_button.grid(row=2, column=1)
+
 
 root = Tk()
 
-my_todolist = Todolist(root, "moja lista")
+my_todolist = Todolist(root, "my to do list")
 
 root.mainloop()
