e9c030982d609720b5ce0a544a71ecaf00a37974
Franciszek <franekkociszewski33@gmail.com>
2020-05-29 16:34:56 +0200

    removing paddings

--- main.py
+++ main.py
@@ -7,30 +7,30 @@ class Todolist:
         
 
 
-        self.task_adding_frame = LabelFrame(self.master, text="add task", padx=30, pady=20)
-        self.task_adding_frame.pack(padx=30, pady=20)
+        self.task_adding_frame = LabelFrame(self.master, text="add task")
+        self.task_adding_frame.pack()
        
-        self.help_label_task_name = Label(self.task_adding_frame, text="task name")
+        self.label_task_name = Label(self.task_adding_frame, text="task name:")
         self.input_task_name_entry = Entry(self.task_adding_frame)
 
-        self.deadline_date_label = Label(self.task_adding_frame, text="deadline date")
+        self.deadline_date_label = Label(self.task_adding_frame, text="deadline date:")
         self.deadline_date_input = Entry(self.task_adding_frame)
 
         self.add_task_button = Button(self.task_adding_frame, text="add task")
 
 
-        self.help_label_task_name.grid(row=0, column=0)
-        self.input_task_name_entry.grid(row=0, column=1)
+        self.label_task_name.grid(row=0, column=0)
+        self.input_task_name_entry.grid(row=1, column=0)
 
-        self.deadline_date_label.grid(row=1, column=0)
-        self.deadline_date_input.grid(row=1, column=1)
+        self.deadline_date_label.grid(row=2, column=0)
+        self.deadline_date_input.grid(row=3, column=0)
 
-        self.add_task_button.grid(row=2, column=1)
+        self.add_task_button.grid(row=4, column=0, pady=10)
 
 
         self.tasks_list_frame = LabelFrame(self.master, text="tasks list")
         self.tasks_list_frame.pack(padx=30, pady=40)
-        
+
 
 root = Tk()
 
