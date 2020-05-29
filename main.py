62267869849fe790037344b29fe82e92fe90e4f4
Franciszek <franekkociszewski33@gmail.com>
2020-05-29 20:20:33 +0200

    add clear Entry function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 
 class Todolist:
-    
 
     def __init__(self, master, window_title, **options):
         self.master = master
@@ -17,6 +16,7 @@ class Todolist:
         self.deadline_date_input = Entry(self.task_adding_frame)
     
         self.add_task_button = Button(self.task_adding_frame, text="add task", command=self.put_task_to_box)
+        
 
         self.label_task_name.grid(row=0, column=0)
         self.input_task_name_entry.grid(row=1, column=0)
@@ -32,9 +32,21 @@ class Todolist:
         self.tasks_box = Listbox(self.tasks_list_frame)
         self.tasks_box.pack()    
 
+        self.delete_task_button = Button(self.tasks_list_frame, text="delete task", command=self.remove_task_from_box)
+        self.delete_task_button.pack()
+
     def put_task_to_box(self):
         self.tasks_box.insert(END, self.input_task_name_entry.get())
         print("add: ", self.input_task_name_entry.get())
+        self.clear_task_name_entry()
+
+    def remove_task_from_box(self):
+        self.tasks_box.delete(ANCHOR)
+        
+    
+    def clear_task_name_entry(self):
+        self.input_task_name_entry.delete(0, END)
+        print("Entry clear")
 
 root = Tk()
 
